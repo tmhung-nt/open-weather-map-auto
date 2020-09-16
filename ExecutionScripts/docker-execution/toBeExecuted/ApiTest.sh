bcdd9d5c17a884a914afb8dbd54a9dde4c6911ce
Hung Tran <tmhung.nt@gmail.com>
2020-09-16 20:20:16 +0700

    add API Modal driven test using Stub to execution script update UI report name

--- ExecutionScripts/docker-execution/toBeExecuted/ApiTest.sh
+++ ExecutionScripts/docker-execution/toBeExecuted/ApiTest.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-pabot -d api_results/  --listener allure_robotframework  --log API_Testing_log.html --report API_Testing_report.html --output API_Testing.xml APIs/Tests
+pabot -d api_results/  --listener allure_robotframework  --log API_Paralle_Testing_log.html --report API_Paralle_Testing_report.html --output API_Paralle_Testing.xml APIs/Tests
+
+pabot -d api_results/ --listener allure_robotframework  --log API_ModalDriven_Testing_log.html --report API_ModalDriven_Testing_report.html --output API_ModalDriven_Testing.xml  -v APP_URL:"http://localhost:7777" APIs/Tests/FindByCityModalDriven.robot
--- ExecutionScripts/docker-execution/toBeExecuted/UiSmokeTestParallel.sh
+++ ExecutionScripts/docker-execution/toBeExecuted/UiSmokeTestParallel.sh
@@ -4,10 +4,10 @@ suite_path="UI/Tests"
 
 reportDir="/tests/out/${suite_path}"
 
-pabot  --testlevelsplit -d ${reportDir}  --listener allure_robotframework  --log Chrome_log.html --report Chrome_report.html --output Chrome_output.xml \
+pabot  --testlevelsplit -d ${reportDir}  --listener allure_robotframework  --log Chrome_Parallel_log.html --report Chrome_Parallel_report.html --output Chrome_Parallel_output.xml \
              -v BROWSER:gc  -v CUR_TEST_NAME:${test_suite_name}_Chrome  ${suite_path}/SearchWeatherByCity.robot 
 
-pabot  --testlevelsplit -d ${reportDir}  --listener allure_robotframework  --log Firefox_log.html --report Firefox_report.html --output Firefox_output.xml \
+pabot  --testlevelsplit -d ${reportDir}  --listener allure_robotframework  --log Firefox_Paralle_log.html --report Firefox_Paralle_report.html --output Firefox_Paralle_output.xml \
              -v BROWSER:ff  -v CUR_TEST_NAME:${test_suite_name}_Firefox  ${suite_path}/SearchWeatherByCity.robot 
 
 robot -d ${reportDir}  --listener allure_robotframework  --log data_driven_log.html --report data_driven_report.html --output data_driven_output.xml \
