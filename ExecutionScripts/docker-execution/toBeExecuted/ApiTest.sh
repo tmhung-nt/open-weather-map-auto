06d28a2e2aa76378e00a607ce52dbfa4769d8fc0
Hung Tran Manh <tranmanhhung@tma.com.vn>
2020-09-16 20:32:00 +0700

    set report directory for API tests

--- ExecutionScripts/docker-execution/toBeExecuted/ApiTest.sh
+++ ExecutionScripts/docker-execution/toBeExecuted/ApiTest.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-pabot -d api_results/  --listener allure_robotframework  --log API_Paralle_Testing_log.html --report API_Paralle_Testing_report.html --output API_Paralle_Testing.xml APIs/Tests
 
-pabot -d api_results/ --listener allure_robotframework  --log API_ModalDriven_Testing_log.html --report API_ModalDriven_Testing_report.html --output API_ModalDriven_Testing.xml  -v APP_URL:"http://localhost:7777" APIs/Tests/FindByCityModalDriven.robot
+reportDir="/tests/out/API"
+
+pabot -d api_results/ -d ${reportDir}  --listener allure_robotframework  --log API_Paralle_Testing_log.html --report API_Paralle_Testing_report.html --output API_Paralle_Testing.xml APIs/Tests
+
+pabot -d api_results/ -d ${reportDir}  --listener allure_robotframework  --log API_ModalDriven_Testing_log.html --report API_ModalDriven_Testing_report.html --output API_ModalDriven_Testing.xml  -v APP_URL:"http://localhost:7777" APIs/Tests/FindByCityModalDriven.robot
